f0f137e3c4bcac31e00245cfe2fd2faa832d9ff6
tamutamu <tamu.0.0.tamu@gmail.com>
2023-09-07 12:29:21 +0900

    不要なので戻す

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    rawBody: true,
-  });
-  app.useBodyParser('json');
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
